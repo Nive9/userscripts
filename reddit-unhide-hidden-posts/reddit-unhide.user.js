9e21d7cbc6ceb6f6bb4cd27e9b82afec6d322f02
Nive9 <29470670+Nive9@users.noreply.github.com>
2022-05-02 02:05:33 +0200

    Update reddit-unhide.user.js

--- reddit-unhide-hidden-posts/reddit-unhide.user.js
+++ reddit-unhide-hidden-posts/reddit-unhide.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Reddit Unhide Hidden Posts
 // @namespace    https://github.com/Nive9/userscripts
-// @version      1.0.1
+// @version      1.0.2
 // @description  Unhides all hidden posts when visiting https://np.reddit.com/user/USERNAME/hidden/ (USERNAME = logged in user's name)
 // @author       Nive
 // @updateURL    https://raw.githubusercontent.com/Nive9/userscripts/reddit-unhide-hidden-posts/main/reddit-unhide.user.js
 // @downloadURL  https://github.com/Nive9/userscripts/raw/main/reddit-unhide-hidden-posts/reddit-unhide.user.js
-// @match        **://np.reddit.com/user/*
+// @match        *://np.reddit.com/user/*
 // @icon         https://icons.duckduckgo.com/ip3/www.reddit.com.ico
 // @grant        none
 // ==/UserScript==
